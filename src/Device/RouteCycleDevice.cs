10e5e6710265006d2debbb9cb501964fe16c1769
Jonathan Arndt <jarndt@pepperdash.com>
2023-09-23 09:17:53 -0700

    WIP: add in custom device classes

--- src/Device/RouteCycleDevice.cs
+++ src/Device/RouteCycleDevice.cs
@@ -1,7 +1,10 @@
 ﻿using Crestron.SimplSharpPro.DeviceSupport;
+using Crestron.SimplSharp;
 using PepperDash.Core;
 using PepperDash.Essentials.Core;
 using PepperDash.Essentials.Core.Bridges;
+using System.Collections.Generic;
+using System.Threading;
 using RouteCycle.Config;
 using RouteCycle.JoinMaps;
 
@@ -20,9 +23,9 @@ namespace RouteCycle.Factories
         /// <summary>
         /// Plugin device constructor
         /// </summary>
-        /// <param name="key"></param>
-        /// <param name="name"></param>
-        /// <param name="config"></param>
+        /// <param name="key">Device unique key</param>
+        /// <param name="name">Device friendly name</param>
+        /// <param name="config">Device configuration</param>
         public RouteCycleDevice(string key, string name, RouteCycleConfigObject config)
             : base(key, name)
         {
@@ -69,6 +72,89 @@ namespace RouteCycle.Factories
             };
         }
         #endregion
+    }
+
+    public class OutputFeedback
+    {
+        public ushort Index { get; set; }
+        public bool IndexEnabled { get; set; }
+        public ushort IndexValue { get; set; }
+        public string IndexLabel { get; set; }
+        public ushort ShiftedIndex { get; set; }
+        public ushort ShiftedIndexValue { get; set; }
+    }
+
+    public class MyPluginBridgeEpi
+    {
+        private CTimer shiftTimer;
+        private List<OutputFeedback> outputFeedbacks = new List<OutputFeedback>();
+
+        //Initialize your timer here and set interval
+        //shiftTimer = new Timer(1000); // 1000 ms = 1 second
+        shiftTimer = new CTimer(ShiftTimer_Elapsed, null, 1000, 1000);  // 1000 ms = 1 second
+        shiftTimer.Elapsed += ShiftTimer_Elapsed;
+
+        // Initialize the OutputFeedbacks collection here
+        for (ushort i = 0; i < 32; i++)
+        {
+            outputFeedbacks.Add(new OutputFeedback
+            {
+                Index = i,
+                IndexEnabled = false,
+                IndexValue = 0,
+                IndexLabel = $"Index {i}",
+                ShiftedIndex = 0,
+                ShiftedIndexValue = 0
+            });
+        }
+        
+        private void ShiftTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            // First loop
+            for (int i = 0; i < outputFeedbacks.Count - 1; i++)
+            {
+                var current = outputFeedbacks[i];
+                var next = outputFeedbacks[i + 1];
+                if (current.IndexEnabled)
+                {
+                    current.ShiftedIndexValue = next.IndexValue;
+                }
+            }
+
+            // Second loop
+            for (int i = 0; i < outputFeedbacks.Count; i++)
+            {
+                var current = outputFeedbacks[i];
+                if (current.IndexEnabled)
+                {
+                    var shiftedItem = outputFeedbacks[current.ShiftedIndex];
+                    shiftedItem.IndexValue = current.IndexValue;
+                    shiftedItem.IndexLabel = current.IndexLabel;
+                }
+            }
+        }
+
+        public void SetTimerEnabled(bool enabled)
+        {
+            if (enabled)
+                shiftTimer.Start();
+            else
+                shiftTimer.Stop();
+        }
+
+        public OutputFeedback GetOutputFeedback(int index)
+        {
+            return outputFeedbacks[index];
+        }
+
+        public void SetOutputFeedback(OutputFeedback feedback)
+        {
+            var item = outputFeedbacks[feedback.Index];
+            item.IndexEnabled = feedback.IndexEnabled;
+            item.IndexValue = feedback.IndexValue;
+            item.IndexLabel = feedback.IndexLabel;
+        }
+
     }
 }
 
